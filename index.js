b217709175e6f07e8f52557ed59a583d4a3a2aad
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2022-05-11 22:34:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -57,104 +57,13 @@ module.exports = function createPlugin(app) {
   };
   plugin.fetchStatus = function fetchStatus(options) {
     const values = [];
-    getData(1, 38, options)
+    getData(1, 2, options)
       .then((data) => {
         const modemUptime = Buffer.concat(data.slice(0, 2)).readUInt32BE();
         values.push({
           path: 'networking.modem.uptime',
           value: modemUptime,
         });
-        const signalStrength = Buffer.concat(data.slice(2, 5)).readInt32BE();
-        values.push({
-          path: 'networking.lte.rssi',
-          value: signalStrength,
-        });
-        const signalBars = Math.min(Math.floor((signalStrength + 100) / 8), 5);
-        values.push({
-          path: 'networking.lte.bars',
-          value: signalBars,
-        });
-        const radioQuality = Math.min((signalStrength + 100) / 8, 5) / 5;
-        values.push({
-          path: 'networking.lte.radioQuality',
-          value: radioQuality,
-        });
-        const modemTemperature = Buffer.concat(data.slice(4, 7)).readInt32BE() / 10 + 273.15;
-        values.push({
-          path: 'networking.modem.temperature',
-          value: modemTemperature,
-        });
-        const operator = Buffer.concat(data.slice(22)).toString().replace(/\0.*$/g, '');
-        values.push({
-          path: 'networking.lte.registerNetworkDisplay',
-          value: operator,
-        });
-        app.setPluginStatus(`Connected to ${operator}, signal strength ${signalStrength}dBm`);
-        return getData(119, 16, options);
-      })
-      .then((data) => {
-        const connectionType = Buffer.concat(data.slice(0, 15)).toString().replace(/\0.*$/g, '');
-        values.push({
-          path: 'networking.lte.connectionText',
-          value: connectionType,
-        });
-        return getData(143, 4, options);
-      })
-      .then((data) => {
-        const modemLat = Buffer.concat(data.slice(0, 2)).readFloatLE();
-        const modemLon = Buffer.concat(data.slice(2, 4)).readFloatLE();
-        values.push({
-          path: 'navigation.position',
-          value: {
-            latitude: modemLat,
-            longitude: modemLon,
-          },
-        });
-
-        return getData(179, 4, options);
-      })
-      .then((data) => {
-        const modemSpeed = Buffer.concat(data.slice(0, 2)).readInt32BE();
-        values.push({
-          path: 'navigation.speedOverGround',
-          value: modemSpeed,
-        });
-
-        const modemSats = Buffer.concat(data.slice(2, 4)).readUInt32BE();
-        values.push({
-          path: 'navigation.gnss.satellites',
-          value: modemSats,
-        });
-
-        return getData(87, 16, options);
-      })
-      .then((data) => {
-        const activeSim = Buffer.concat(data.slice(0, 15)).toString();
-        switch (activeSim.slice(0, 4)) {
-          case 'sim2': {
-            return getData(300, 4, options);
-          }
-          default: {
-            if (options.RUT240) {
-              return getData(135, 4, options);
-            }
-            return getData(185, 4, options);
-          }
-        }
-      })
-      .then((data) => {
-        const rx = Buffer.concat([data[0], data[1]]).readUInt32BE();
-        const tx = Buffer.concat([data[2], data[3]]).readUInt32BE();
-        values.push(
-          {
-            path: 'networking.lte.usage.tx',
-            value: tx,
-          },
-          {
-            path: 'networking.lte.usage.rx',
-            value: rx,
-          },
-        );
       })
       .then(() => {
         app.handleMessage(plugin.id, {
@@ -170,9 +79,9 @@ module.exports = function createPlugin(app) {
           ],
         });
       })
-//      .catch((err) => {
-//        app.setPluginError(err.message);
-//      });
+      .catch((err) => {
+        app.setPluginError(err.message);
+      });
 
     timeout = setTimeout(() => {
       plugin.fetchStatus(options);
